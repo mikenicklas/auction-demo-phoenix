0cf480a893434d8c4340f075d1f13443156eedf8
Mike Nicklas <mike.nicklas@gmail.com>
2018-09-24 07:09:38 -0400

    End of ch 5

---
+++ apps/auction/mix.exs
@@ -0,0 +1,33 @@
+defmodule Auction.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :auction,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Auction.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto, "~> 2.2.10"},
+      {:postgrex, "~> 0.13.3"}
+    ]
+  end
+end
